77072d95a916dde89f6b09025a927963e86467ab
Tom Wechsler <tom.wechsler@outlook.com>
2026-07-06 06:47:55 +0200

    RAG Examples

---
+++ AI_Testlab/RAG_Examples/basic_rag_part2.py
@@ -0,0 +1,35 @@
+# This script demonstrates how to use the LangChain Community VectorStores module to retrieve relevant documents based on a user's question.
+
+
+# importing the required libraries
+import os
+
+from langchain_chroma import Chroma 
+from langchain_ollama import OllamaEmbeddings
+
+# Define the persistent directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(current_dir, "db", "chroma_db_security")
+
+embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+# Load the existing vector store with the embedding function
+db = Chroma(persist_directory=persistent_directory,
+            embedding_function=embeddings)
+
+# Define the user's question
+query = "What is SSRF? Provide an example of an SSRF attack."
+
+# Retrieve relevant documents based on the query
+retriever = db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 5, "score_threshold": 0.5},
+)
+relevant_docs = retriever.invoke(query)
+
+# Display the relevant results with metadata
+print("\n--- Relevant Documents ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+    if doc.metadata:
+        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
